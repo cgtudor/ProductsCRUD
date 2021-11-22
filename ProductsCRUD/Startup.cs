71e51bbca69b46b8185371519b561d2c09aed599
V8002382 <V8002382@TU46993>
2021-11-22 15:37:54 +0000

    Swagger+auth0

--- ProductsCRUD/Startup.cs
+++ ProductsCRUD/Startup.cs
@@ -15,6 +15,10 @@ using ProductsCRUD.Repositories.Interface;
 using ProductsCRUD.Repositories.Concrete;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Reflection;
+using System.IO;
 
 namespace ProductsCRUD
 {
@@ -42,6 +46,16 @@ namespace ProductsCRUD
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["Auth0:Domain"];
+                options.Audience = Configuration["Auth0:Audience"];
+            });
+
             /*if(_environment.IsDevelopment())
             {
             }
@@ -51,10 +65,43 @@ namespace ProductsCRUD
 
             services.AddSingleton<IProductsRepository, FakeProductsRepository>();
 
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ProductsCRUD API",
+                    Description = "An ASP.NET Core Web API for managing products of ThAmCo."
+                });
+
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "openid", "Open Id" }
+                            },
+                            AuthorizationUrl = new Uri(Configuration["Auth0:Domain"] + "authorize?audience=" + Configuration["Auth0:Audience"])
+                        }
+                    }
+                });
+            });
+
             services.AddMvc(options =>
             {
                 options.SuppressAsyncSuffixInActionNames = false;
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,11 +110,19 @@ namespace ProductsCRUD
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+                    options.OAuthClientId(Configuration["Auth0:ClientId"]);
+                });
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
